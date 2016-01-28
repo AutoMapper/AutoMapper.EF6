8c5350cb34d9859587bc9c7cde61fd86eb8d9d69
Jimmy Bogard <jimmy.bogard@gmail.com>
2016-01-28 09:16:57 -0600

    Updating to non-static API of AutoMapper

--- src/AutoMapper.EF6/EntityFrameworkExtensions.cs
+++ src/AutoMapper.EF6/EntityFrameworkExtensions.cs
@@ -10,69 +10,69 @@
 
     public static class EntityFrameworkExtensions
     {
-        public static async Task<List<TDestination>> ProjectToListAsync<TDestination>(this IQueryable queryable)
+        public static async Task<List<TDestination>> ProjectToListAsync<TDestination>(this IQueryable queryable, IConfigurationProvider config)
         {
-            return await queryable.ProjectTo<TDestination>().DecompileAsync().ToListAsync();
+            return await queryable.ProjectTo<TDestination>(config).DecompileAsync().ToListAsync();
         }
 
-        public static async Task<TDestination[]> ProjectToArrayAsync<TDestination>(this IQueryable queryable)
+        public static async Task<TDestination[]> ProjectToArrayAsync<TDestination>(this IQueryable queryable, IConfigurationProvider config)
         {
-            return await queryable.ProjectTo<TDestination>().DecompileAsync().ToArrayAsync();
+            return await queryable.ProjectTo<TDestination>(config).DecompileAsync().ToArrayAsync();
         }
 
-        public static async Task<TDestination> ProjectToSingleOrDefaultAsync<TDestination>(this IQueryable queryable)
+        public static async Task<TDestination> ProjectToSingleOrDefaultAsync<TDestination>(this IQueryable queryable, IConfigurationProvider config)
         {
-            return await queryable.ProjectTo<TDestination>().DecompileAsync().SingleOrDefaultAsync();
+            return await queryable.ProjectTo<TDestination>(config).DecompileAsync().SingleOrDefaultAsync();
         }
 
-        public static async Task<TDestination> ProjectToSingleAsync<TDestination>(this IQueryable queryable)
+        public static async Task<TDestination> ProjectToSingleAsync<TDestination>(this IQueryable queryable, IConfigurationProvider config)
         {
-            return await queryable.ProjectTo<TDestination>().DecompileAsync().SingleAsync();
+            return await queryable.ProjectTo<TDestination>(config).DecompileAsync().SingleAsync();
         }
 
-        public static async Task<TDestination> ProjectToFirstOrDefaultAsync<TDestination>(this IQueryable queryable)
+        public static async Task<TDestination> ProjectToFirstOrDefaultAsync<TDestination>(this IQueryable queryable, IConfigurationProvider config)
         {
-            return await queryable.ProjectTo<TDestination>().DecompileAsync().FirstOrDefaultAsync();
+            return await queryable.ProjectTo<TDestination>(config).DecompileAsync().FirstOrDefaultAsync();
         }
 
-        public static async Task<TDestination> ProjectToFirstAsync<TDestination>(this IQueryable queryable)
+        public static async Task<TDestination> ProjectToFirstAsync<TDestination>(this IQueryable queryable, IConfigurationProvider config)
         {
-            return await queryable.ProjectTo<TDestination>().DecompileAsync().FirstAsync();
+            return await queryable.ProjectTo<TDestination>(config).DecompileAsync().FirstAsync();
         }
 
-        public static List<TDestination> ProjectToList<TDestination>(this IQueryable queryable)
+        public static List<TDestination> ProjectToList<TDestination>(this IQueryable queryable, IConfigurationProvider config)
         {
-            return queryable.ProjectTo<TDestination>().Decompile().ToList();
+            return queryable.ProjectTo<TDestination>(config).Decompile().ToList();
         }
 
-        public static TDestination[] ProjectToArray<TDestination>(this IQueryable queryable)
+        public static TDestination[] ProjectToArray<TDestination>(this IQueryable queryable, IConfigurationProvider config)
         {
-            return queryable.ProjectTo<TDestination>().Decompile().ToArray();
+            return queryable.ProjectTo<TDestination>(config).Decompile().ToArray();
         }
 
-        public static TDestination ProjectToSingleOrDefault<TDestination>(this IQueryable queryable)
+        public static TDestination ProjectToSingleOrDefault<TDestination>(this IQueryable queryable, IConfigurationProvider config)
         {
-            return queryable.ProjectTo<TDestination>().Decompile().SingleOrDefault();
+            return queryable.ProjectTo<TDestination>(config).Decompile().SingleOrDefault();
         }
 
-        public static TDestination ProjectToSingle<TDestination>(this IQueryable queryable)
+        public static TDestination ProjectToSingle<TDestination>(this IQueryable queryable, IConfigurationProvider config)
         {
-            return queryable.ProjectTo<TDestination>().Decompile().Single();
+            return queryable.ProjectTo<TDestination>(config).Decompile().Single();
         }
 
-        public static TDestination ProjectToFirstOrDefault<TDestination>(this IQueryable queryable)
+        public static TDestination ProjectToFirstOrDefault<TDestination>(this IQueryable queryable, IConfigurationProvider config)
         {
-            return queryable.ProjectTo<TDestination>().Decompile().FirstOrDefault();
+            return queryable.ProjectTo<TDestination>(config).Decompile().FirstOrDefault();
         }
 
-        public static TDestination ProjectToFirst<TDestination>(this IQueryable queryable)
+        public static TDestination ProjectToFirst<TDestination>(this IQueryable queryable, IConfigurationProvider config)
         {
-            return queryable.ProjectTo<TDestination>().Decompile().First();
+            return queryable.ProjectTo<TDestination>(config).Decompile().First();
         }
 
-        public static IQueryable<TDestination> ProjectToQueryable<TDestination>(this IQueryable queryable)
+        public static IQueryable<TDestination> ProjectToQueryable<TDestination>(this IQueryable queryable, IConfigurationProvider config)
         {
-            return queryable.ProjectTo<TDestination>().Decompile();
+            return queryable.ProjectTo<TDestination>(config).Decompile();
         }
     }
 }
